b9e707130de3cd7c99b6b65a97c558d1fb67a550
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-26 16:48:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from database import LocalDatabase
 
+db = LocalDatabase()
+
 st.title("🎈 My new app Hii")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
