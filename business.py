82e73822d607c16eb1ee4c3f556865b609133aef
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-27 15:39:55 +0530

    Update business.py

--- business.py
+++ business.py
@@ -6,5 +6,6 @@ class DataServer:
     model_name = "india_gdp_forecasting_model.pkl"
   ):
     self.model = joblib.load(model_name)
-def makeForecast(self):
-  return self.model
+    
+  def makeForecast(self):
+    return self.model
