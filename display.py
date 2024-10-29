be2287a70c57a0865e188b5914df607c22928910
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-29 14:26:27 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 from bussiness import Model
 import streamlit as st
+import plotly.express as px
 
 class IndiaGDPApp:
   def __init__(
@@ -8,4 +9,16 @@ class IndiaGDPApp:
     self.model = Model()
 
   def forecast(self, years):
-    forcast_data =  model.makeForecast()
+    forcast_data =  model.makeForecast(years = range(1, 11))
+    
+    # Plot India's GDP
+    fig = px.line(df.reset_index(), x='Year', y='GDP', 
+                   title="India's GDP from 1961 to 2023",
+                   labels={'GDP': 'GDP (in Trillions of USD)', 'Year': 'Year'},
+                   markers=True
+                  )
+    # Show the plot
+    #st.plotly_chart(fig)
+    return fig
+
+
