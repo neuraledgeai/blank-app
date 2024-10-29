7171f6ed0ab965e2373360d9d25bdf1ea765585c
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-29 13:58:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ from business import Model
 #df = db.getDataFrame()
 
 model = Model()
-forcasted_df = model.makeForecast(years = range(1, 1))
+# When it is 1, it means 0. 
+forcasted_df = model.makeForecast(years = range(1, 3))
 st.title(f"From Data Server okay")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
