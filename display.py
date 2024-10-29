eb519cf6b0c506f2a9f7458718a3882479584382
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-29 14:21:34 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,1 +1,11 @@
+from bussiness import Model
+import streamlit as st
 
+class IndiaGDPApp:
+  def __init__(
+    #model = Model()
+  ):
+    self.model = Model()
+
+  def forecast(self, years):
+    forcast_data =  model.makeForecast()
