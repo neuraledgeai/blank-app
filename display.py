c4130826e487298799d05b639d56d8cbc3998d53
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-29 15:20:51 +0530

    Update display.py

--- display.py
+++ display.py
@@ -11,13 +11,7 @@ class IndiaGDPApp:
     self.model = model
 
   def forecast(self, years):
-    df =  self.model.makeForecast(years = range(1, 11))
-    
-    # Plot India's GDP
-    #fig = px.line(df, x=df.index, y='GDP', title='GDP Over the Years')
-    # Show the plot
-    #st.plotly_chart(fig)
-    plot = df.plot()
-    return plot
+    fig =  self.model.makeForecast(years = range(1, 11))
+    st.plotly_chart(fig)
 
 
