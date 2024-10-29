ca8a705c286c87a886bd2dfa31d5f5e103d8b8d0
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-29 13:17:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from database import LocalDatabase
-from business import DataServer
+from business import Model
 
 #db = LocalDatabase()
 #df = db.getDataFrame()
 
 ds = DataServer()
 model = ds.makeForecast()
-st.title(f"From Data Serverjjjjj: {model}")
+st.title(f"From Data Serverjjjjj: {model}, okay")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
