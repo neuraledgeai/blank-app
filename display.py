73957d1e8c5c0e468405faf174afdd96d67ee1ea
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-30 15:26:50 +0530

    Update display.py

--- display.py
+++ display.py
@@ -11,22 +11,17 @@ class PresentationComponents:
     self.model = model
 
   def forecast_bar_chart(self,years):
-    
-    # Get the data frame
+    # Get dataframe
     df =  self.model.makeForecast(years = range(1, years))
-    
+
+    # Plot figure
     fig = px.bar(df, x="Year", y="GDP", title=f"GDP Forecast Over the Next {years-1} Years")
-    #fig1 = px.line(df, x="Year", y="GDP", title="GDP Forecast Over the Years")
     st.plotly_chart(fig)
-    #st.plotly_chart(fig1)
 
   def forecast_line_chart(self, years):
-    # Get the data frame
+    # Get dataframe
     df =  self.model.makeForecast(years = range(1, years))
-    
-    # Header
-    st.subheader("GDP Forecast :blue[Line Chart]")
 
-    # Plot the figure
-    fig = px.line(df, x="Year", y="GDP", title="GDP Forecast Over the Years")
+    # Plot figure
+    fig = px.line(df, x="Year", y="GDP", title=f"GDP Forecast Over the Next {years-1} Years")
     st.plotly_chart(fig)
