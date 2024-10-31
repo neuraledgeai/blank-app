f016920ab16ba27e843c5510f33e071cf691f5e3
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-31 16:59:44 +0530

    Update display.py

--- display.py
+++ display.py
@@ -2,6 +2,7 @@ from business import Model
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class PresentationComponents:
   def __init__(
