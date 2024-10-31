dfbd2c7d5f759a002b1f1d429d5a2c0c6a88a36c
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-31 15:51:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,10 @@ from business import Model
 from display import PresentationComponents
 
 #pc = PresentationComponents()
-db = LocalDatabase()
+#db = LocalDatabase()
 #df = db.getDataFrame()
 
-df = db.loadData()
-#model = Model()
+model = Model()
 # When it is 1, it means 0. 
 #forcasted_df = model.makeForecast(years = range(1, 11))
 
@@ -17,7 +16,7 @@ st.title(f"From Data Server {df.dtypes} okay sjjsss")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.dataframe(df)
+st.dataframe(model.predict())
 #app.forecast(years = 11)
 #st.subheader("GDP Forecast :blue[Bar Chart]")
 #years = st.slider("Forecast Horizon", 0, 20, 5)
