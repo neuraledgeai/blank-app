b1b5ad1034d929add56c4e5a8c75224a2cf6b9fd
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-10-31 15:35:36 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,18 +8,18 @@ class LocalDatabase:
   ):
     self.df = pd.read_csv(file_name)
     
-  def getDataFrame(self):
-    dataFrame = pd.melt(
+  def loadData(self):
+    df = pd.melt(
         self.df,
         id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],  # Columns to keep as identifiers
         var_name='Year',      # The name of the new column for the years
         value_name='GDP'      # The name of the new column for the GDP values
     )
     # Setting the 'Year' column as the index and dropping unnecessary columns
-    dataFrame = dataFrame.set_index("Year").drop(columns=["Country Name", "Country Code", "Indicator Name", "Indicator Code"])
+    df = df.drop(columns=["Country Name", "Country Code", "Indicator Name", "Indicator Code"])
     # Adding a new feature for GDP_L1
-    dataFrame['GDP_L1'] = dataFrame['GDP'].shift(1)
-    dataFrame = dataFrame.dropna()  # Drop rows with NaN values resulting from the shift
+    #dataFrame['GDP_L1'] = dataFrame['GDP'].shift(1)
+    $dataFrame = dataFrame.dropna()  # Drop rows with NaN values resulting from the shift
 
-    return dataFrame
+    return df
   
