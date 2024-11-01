e8050d5a427ff65bb6fd2ae82fe677f361734c11
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 17:05:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ pc = PresentationComponents()
 #pc.forecast_bar_chart(years = years+1)
 #pc.downloadData(years = years+1)
 
-st.sidebar.header("Navigate the Dashboard Ye")
+st.sidebar.header("Navigate the Dashboard Yes")
 navigation = st.sidebar.radio(
     "Dive into the Data!",
     ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Model Performance", "Download Forecasted Data"],
@@ -23,15 +23,15 @@ navigation = st.sidebar.radio(
 # Home section
 if navigation == "GDP Forecast :blue[Bar Chart]":
     st.subheader("GDP Forecast :blue[Bar Chart]")
-    years_bar_chart = st.slider("Forecast Horizon", 0, 20, 5)
-    pc.forecast_bar_chart(years = years_bar_chart+1)
+    years = st.slider("Forecast Horizon", 0, 20, 5)
+    pc.forecast_bar_chart(years = years+1)
 elif navigation == "GDP Forecast :blue[Line Chart]":
     st.subheader("GDP Forecast :blue[Line Chart]")
-    years_line_chart = st.slider("Forecast Horizon", 0, 20, 5)
-    pc.forecast_line_chart(years = years_line_chart+1)
+    years = st.slider("Forecast Horizon", 0, 20, 5)
+    pc.forecast_line_chart(years = years+1)
 elif navigation == "Model Performance":
     pc.modelPerformance()
 elif navigation == "Download Forecasted Data":
     #st.subheader("GDP Forecast :blue[Line Chart]")
-    years_df = st.slider("Forecast Horizon", 0, 20, 5)
-    pc.downloadData(years = years_df+1)
+    years = st.slider("Forecast Horizon", 0, 20, 5)
+    pc.downloadData(years = years+1)
