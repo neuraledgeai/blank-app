fafa68aaec121187e4dbd467c13fc833c811c7ac
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 15:40:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from display import PresentationComponents
 st.sidebar.header("Navigate the Dashboard")
 navigation = st.sidebar.radio(
     "What's your favorite movie genre",
-    ["GDP Forecast :blue[Bar Chart]", "GDP Forecast blue[Line Chart]", "Download Forecast Data :download:"],
+    ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Download Forecast Data"],
     captions=[
         "Laugh out loud.",
         "Get the popcorn.",
