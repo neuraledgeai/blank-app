68babde4acc7dd1e77014a63d693a69981b54139
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 15:31:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from display import PresentationComponents
 #pc.downloadData(years = years+1)
 
 st.sidebar.header("Navigate the Dashboard")
-navigation = st.radio(
+navigation = st.sidebar.radio(
     "What's your favorite movie genre",
     [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
     captions=[
