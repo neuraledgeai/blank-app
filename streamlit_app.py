fd38d1f79fd6a65814c21a5e16bf790a24a8a4f2
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 16:15:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,3 @@ if navigation == "GDP Forecast :blue[Bar Chart]":
     st.subheader("GDP Forecast :blue[Bar Chart]")
     years = st.slider("Forecast Horizon", 0, 20, 5)
     pc.forecast_bar_chart(years = years+1)
-    st.write(f"The Bar Chart provides a visual representation of India’s projected GDP growth over the next {years} years. Each bar represents a specific year's forecasted GDP value, displayed in trillions of US dollars.")
-    st.markdown(''':blue-background[Highlight] : India is expected to touch 5 trillion in GDP at current prices by 2030.''')
-    st.markdown("""
-    <div style="text-align: center; font-size: 0.8em; color: grey;">
-    The model is trained on data only up to 2023, so predictions may vary due to future uncertainties. Please verify important information independently.
-    </div>
-    """, unsafe_allow_html=True)
