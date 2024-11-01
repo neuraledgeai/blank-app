96444868edebb3bf7b41ed6e41ca7447c1c1756c
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 17:00:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ pc = PresentationComponents()
 st.sidebar.header("Navigate the Dashboard Yes")
 navigation = st.sidebar.radio(
     "Dive into the Data!",
-    ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Model Performance"],
+    ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Model Performance", "Download Forecasted Data"],
     captions=[
         "Visual Representation",
         "Visual Representation.",
@@ -29,6 +29,8 @@ elif navigation == "GDP Forecast :blue[Line Chart]":
     years = st.slider("Forecast Horizon", 0, 20, 5)
     pc.forecast_line_chart(years = years+1)
 elif navigation == "Model Performance":
-    #st.subheader("GDP Forecast :blue[Line Chart]")
-    #years = st.slider("Forecast Horizon", 0, 20, 5)
     pc.modelPerformance()
+elif navigation == "Download Forecasted Data":
+    #st.subheader("GDP Forecast :blue[Line Chart]")
+    years = st.slider("Forecast Horizon", 0, 20, 5)
+    pc.downloadData(years = years+1)
