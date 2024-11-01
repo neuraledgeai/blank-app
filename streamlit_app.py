0a5dfa1f011d669be962321cc8d0d317c1516e0d
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-01 16:51:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ pc = PresentationComponents()
 #pc.forecast_bar_chart(years = years+1)
 #pc.downloadData(years = years+1)
 
-st.sidebar.header("Navigate the Dashboard")
+st.sidebar.header("Navigate the Dashboard Yes")
 navigation = st.sidebar.radio(
     "What's your favorite movie genre",
     ["GDP Forecast :blue[Bar Chart]", "GDP Forecast :blue[Line Chart]", "Model Performance"],
