d01283e5703de632344f7006d6eed6dff0f62bac
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-02 11:32:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ class LocalDatabase:
   ):
     self.df = pd.read_csv(file_name)
     
-  def loadData(self):
+  def loadData(self, lag=False):
     df = pd.melt(
         self.df,
         id_vars=["Country Name", "Country Code", "Indicator Name", "Indicator Code"],  # Columns to keep as identifiers
@@ -16,12 +16,14 @@ class LocalDatabase:
         value_name='GDP'      # The name of the new column for the GDP values
     )
     df = df.set_index("Year").drop(columns=["Country Name", "Country Code", "Indicator Name", "Indicator Code"])
-    # Adding a new feature for GDP_L1
-    df["GDP_L1"] = df["GDP"].shift(1)
-    df = df.dropna()  # Drop rows with NaN values resulting from the shift
+    if lag:
+      # Adding a new feature for GDP_L1
+      df["GDP_L1"] = df["GDP"].shift(1)
+      df = df.dropna()  # Drop rows with NaN values resulting from the shift
+      df["GDP_L1"] = df["GDP_L1"].astype(float)
 
     df["GDP"] = df["GDP"].astype(float)
-    df["GDP_L1"] = df["GDP_L1"].astype(float)
+    
     
     return df
   
