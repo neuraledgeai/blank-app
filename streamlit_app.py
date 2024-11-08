ae5c7d724b04509e5ce7eb577978ee532882da46
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-08 14:25:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 
 pc = PresentationComponents()
 
-st.sidebar.header("Navigate the Dashboard")
+st.sidebar.header("Navigate the Dashboard shs")
 navigation = st.sidebar.radio(
     "Dive into the Data!",
     ["GDP Forecast", "Resilient Economy", "Model Performance", "Download Forecasted Data"],
