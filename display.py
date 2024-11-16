d2a21f1fb4da49d4411e979242f539516b43808b
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-16 14:40:28 +0530

    Update display.py

--- display.py
+++ display.py
@@ -53,15 +53,27 @@ class PresentationComponents:
     """, unsafe_allow_html=True)
 
   def empericalResults(self):
+    
+    # Get intercept and coefficient 
+    intercept = self.model.intercept()
+    coefficient = self.model.coef()
+    
     # Get dataframe
     df = self.model.predict()
+
+    # Get model fitted figure
+    fig_fit = self.model.get_fitted_figure()
 
     # Reset the index to make 'Year' a column
     df = df.reset_index()
     
     # Subheader
-    st.subheader("Model Performance")
-    
+    st.subheader("Emperical Results")
+    st.write("The goal of this empirical analysis is to forecast India’s GDP growth trajectory and estimate the specific year in which India is likely to reach a GDP of $5 trillion. To do so, we estimate a linear regression function of the form")
+    st.latex(r"\hat{y}_i = \hat{\beta}_0 + \hat{\beta}_1 \cdot x_i")
+    st.write(f"The independent variable is the GDP value from the previous year, which we use to forecast future values. For example, if you provide the 2023 GDP value as the independent variable, the model will estimate the 2024 GDP based on the estimated intercept *{intercept}* and estimated slope *{coefficient}*.")
+    st.write("Each point represents the GDP of a given year plotted against the GDP of the previous year on the x-axis. The blue line represents the linear regression model fitted to this data.")
+    st.plotly_chart(fig_fit)
     # Plot figure
     fig = px.line(df, x="Year", y=["GDP", "Predicted GDP"], title="Actual vs Predicted GDP Over Time")
     fig.update_layout(xaxis_title="Year", yaxis_title="GDP (in Trillions)")
