7b3b44a9d0cf2290c57a374294b2df4f627ba5cd
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-16 14:31:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,25 +31,25 @@ if navigation == "GDP Forecast":
     pc.forecast_primary_chart(years = years+1)
     pc.forecast_bar_chart(years = years+1)
 elif navigation == "Model Performance":
-    pc.modelPerformance()
+    pc.empericalResults()
 elif navigation == "Download Forecasted Data":
     years = st.slider("Forecast Horizon", 0, 20, 7)
     pc.downloadData(years = years+1)
 elif navigation == "Resilient Economy":
-    model = Model()
-    intercept = model.intercept()
-    coefficient = model.coef()
-    df, fig = model.regressionChart()
+    #model = Model()
+    #intercept = model.intercept()
+    #coefficient = model.coef()
+    #df, fig = model.regressionChart()
     pc.resilientEconomy()
-    st.subheader("Emperical Results")
-    st.write("The goal of this empirical analysis is to forecast India’s GDP growth trajectory and estimate the specific year in which India is likely to reach a GDP of $5 trillion. To do so, we estimate a linear regression function of the form")
+    #st.subheader("Emperical Results")
+    #st.write("The goal of this empirical analysis is to forecast India’s GDP growth trajectory and estimate the specific year in which India is likely to reach a GDP of $5 trillion. To do so, we estimate a linear regression function of the form")
     #st.latex(r"\hat{y}_i = \hat{\beta}_0 + \hat{\beta}_1 \cdot x_i")
-    st.latex(r"{\text{GDP}_{\text{next year}}} = {\beta}_0 + {\beta}_1 \cdot \text{GDP}_{\text{previous year}}")
-    st.write(f"The independent variable is the GDP value from the previous year, which we use to forecast future values. For example, if you provide the 2023 GDP value as the independent variable, the model will estimate the 2024 GDP based on the estimated intercept *{intercept}* and estimated slope *{coefficient}*.")
+    #st.latex(r"{\text{GDP}_{\text{next year}}} = {\beta}_0 + {\beta}_1 \cdot \text{GDP}_{\text{previous year}}")
+    #st.write(f"The independent variable is the GDP value from the previous year, which we use to forecast future values. For example, if you provide the 2023 GDP value as the independent variable, the model will estimate the 2024 GDP based on the estimated intercept *{intercept}* and estimated slope *{coefficient}*.")
     #st.write(f"This is intercept {intercept} and this is coefficient {coefficient[0]}")
     #st.dataframe(df)
-    st.plotly_chart(fig)
-    st.write("Each point represents the GDP of a given year plotted against the GDP of the previous year on the x-axis. The blue line represents the linear regression model fitted to this data.")
+    #st.plotly_chart(fig)
+    #st.write("Each point represents the GDP of a given year plotted against the GDP of the previous year on the x-axis. The blue line represents the linear regression model fitted to this data.")
 
 
    
