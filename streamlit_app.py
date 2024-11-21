591ec3c2d1a32e75f480cbfe47394f5c1bdb42bb
Neural Edge AI <64458507+neuraledgeai@users.noreply.github.com>
2024-11-21 17:39:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ with st.sidebar:
     selected = option_menu(
         "GDP-PY", ["Forecast GDP", "Resilient Economy",  "Emperical Results", "Download Data"], 
         icons=["bar-chart-fill", "ubuntu", "card-text", "download"],
-        menu_icon="graph-up",
+        menu_icon="window",
         default_index=1
     )
     #selected
